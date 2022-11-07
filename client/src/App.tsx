67b6fef6d0518ba0377fea727a55aa185df50abc
wesiudev <wesselpawel@wp.pl>
2022-11-07 23:48:27 +0100

    add loader

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,21 @@ import Contact from "./components/Contact/Contact";
 import Shop from "./components/Shop/Shop";
 import Gallery from "./components/Gallery/Gallery";
 import { ParallaxProvider } from 'react-scroll-parallax';
+import { useState } from "react";
+import Loader from "./components/Loader/Loader";
 export default function App(){
+
+  const [isFirstLoad, setIsFirstLoad] = useState<boolean>(true);
+
+  setTimeout(() => {
+    setIsFirstLoad(false)
+  }, 2800);
 
   return (
     <ParallaxProvider>
       <BrowserRouter>
       <Header/>
+      {isFirstLoad ? <Loader visible={true}/> : <Loader visible={false}/>}
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/gallery" exact component={Gallery} />
